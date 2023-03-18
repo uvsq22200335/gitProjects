a3ebe94d0ad9d2b1d76567d9b1853be43d82c89e
Roman DE CRECY <roman.de-crecy@ens.uvsq.fr>
2023-03-18 12:57:59 +0100

    Correction de la syntaxe

--- #TKINTER BLACKJACK.py
+++ #TKINTER BLACKJACK.py
@@ -1,4 +1,4 @@
-#TKINTER BLACKJACK
+# TKINTER BLACKJACK
 from tkinter import *
 
 
@@ -11,41 +11,30 @@ def start_multi_game():
 
 
 def start_game():
-         
+
     fenetre = Tk()
     fenetre.title("BLACK JACK")
-    #construction du tapis 
-    h, l = 600, 800         
-    tapis = Canvas(fenetre, width = l, height= h, background= "#006400")
+    # construction du tapis
+    h, l = 600, 800
+    tapis = Canvas(fenetre, width=l, height=h, background="#006400")
     tapis.pack()
 
-
-    #cree un canvas pour afficher le texte de la question 
-    question_canvas = Canvas(fenetre, width= 50, height= 50)
-    question_canvas.create_text(250, 25, text = "Choisissez le mode de jeu")
+    # cree un canvas pour afficher le texte de la question
+    question_canvas = Canvas(fenetre, width=50, height=50)
+    question_canvas.create_text(250, 25, text="Choisissez le mode de jeu")
     question_canvas.pack()
 
+    # crée deux boutons pour choisir le mode solo ou multijoueur(le mode multijoueur n'étant pas encore codé)
+    solo_button = Button(fenetre, text="Solo", command=start_solo_game, width=50, height=30)
 
-    #crée deux boutons pour choisir le mode solo ou multijoueur(le mode multijoueur n'étant pas encore codé)        
-    solo_button = Button(fenetre, text = "Solo", command = start_solo_game, width = 50, height= 30)
-    
+    multi_button = Button(fenetre, text="Multijoueur", command=start_multi_game, width=50, height=30)
 
-    multi_button= Button(fenetre, text = "Multijoueur", command = start_multi_game, width= 50, height = 30)
- 
-    
-    #place les boutons sous le canevas
-    solo_button.grid(column=0, row=1, padx = 10, pady = 10)
-    multi_button.grid(column=1, row = 1, padx=10, pady = 10)
-
+    # place les boutons sous le canevas
+    solo_button.grid(column=0, row=1, padx=10, pady=10)
+    multi_button.grid(column=1, row=1, padx=10, pady=10)
 
     fenetre.mainloop()
 
 
-
-
-#lance la fonction pour commencer la partie
+# lance la fonction pour commencer la partie
 start_game()
-
-
-
-
